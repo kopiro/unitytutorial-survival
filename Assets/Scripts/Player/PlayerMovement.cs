0e38e857388c72758bf24cb0258437d2efeca9ac
Flavio De Stefano <destefano.flavio@gmail.com>
2016-09-18 14:58:09 +0200

    fc

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float speed = 6f;
+
+	Vector3 movement;
+	Animator anim;
+	Rigidbody rb;
+
+	int floorMask;
+	float camRayLength = 100f;
+
+	void Awake() {
+		floorMask = LayerMask.GetMask ("Floor");
+		anim = GetComponent<Animator> ();
+		rb = GetComponent<Rigidbody> ();
+	}
+			
+
+
+
+}
